1216b65f4481e8663fcecd2d4c8aa1c76464e1cf
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-12 15:33:53 +0545

    gwtting HTML from URL

--- html_scraper/amazon_scraper.py
+++ html_scraper/amazon_scraper.py
@@ -5,10 +5,26 @@ import requests
 import csv
 import bs4
 
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3"
+REQUEST_HEADER = {
+    'USer-Agent': USER_AGENT,
+    'Accept-Language': 'en-US, en;q=0.5',
+}
+def get_page_html(url):
+    res = requests.get(url= url,headers=REQUEST_HEADER)
+    return res.content
+
+def extract_product_info(url):
+    product_info = {}
+    print(f'Scraping URL: {url}')
+    html = get_page_html(url = url)
+    print(html)
+
 if __name__ == "__main__":
     with open('amazon_products_url.csv',newline='') as csvfile:
         reader = csv.reader(csvfile,delimiter=',')
         for row in reader:
             url = row[0]
-            print(url)
+            # print(url)
+            print(extract_product_info(url))
 
