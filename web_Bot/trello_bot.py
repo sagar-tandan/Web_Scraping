e0d00f2a084a670825d4de5a3c1c5bcbfef0df66
Sagar Tandan <076bct072@ioepc.edu.np>
2024-07-14 15:41:12 +0545

    monir adjustments

--- web_Bot/trello_bot.py
+++ web_Bot/trello_bot.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 from datetime import date
 import os
--- youtube_search/youtube_search.py
+++ youtube_search/youtube_search.py
@@ -1,13 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 from datetime import date
 import os
 import json
 
+service  = Service(executable_path='chromedriver.exe') 
+DRIVER = webdriver.Chrome(service=service)
+
+def searchBar():
+    searchField = DRIVER.find_element(By.CSS_SELECTOR, value= "input[id='search']")
+    searchField.clear();
+    searchField.send_keys('Bahubali OST')
+    
+
+
+def open_youtube():
+    DRIVER.get('https://youtube.com')
+    time.sleep(2)
+    searchBar()
+    time.sleep(4)
+    input("Bot operation Completed. Press any Key...")
+    DRIVER.close()
+    
+    
+
 
 
 if __name__ == "__main__":
-    print("hello")
+    open_youtube()
  
